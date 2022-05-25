43cada591327f0ef40f47b6140af34350547801b
Денис Бессалый <den_bessal@outlook.com>
2022-05-25 16:22:35 +0300

    Добавьте файлы проекта.

---
+++ Polly.Caching.Providers/MemoryCacheProvider.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+
+namespace Polly.Caching
+{
+    public class MemoryCacheProvider : ISyncCacheProvider, IAsyncCacheProvider
+    {
+        private readonly IMemoryCache _cache;
+
+        public MemoryCacheProvider(IMemoryCache memoryCache)
+        {
+            _cache = memoryCache ?? throw new ArgumentNullException(nameof(memoryCache));
+        }
+
+        public (bool, object) TryGet(string key)
+        {
+            bool cacheHit = _cache.TryGetValue(key, out var value);
+            return (cacheHit, value);
+        }
+
+        public void Put(string key, object value, Ttl ttl)
+        {
+            var remaining = DateTimeOffset.MaxValue - DateTimeOffset.UtcNow;
+            var options = new MemoryCacheEntryOptions();
+
+            if (ttl.SlidingExpiration)
+            {
+                options.SlidingExpiration = ttl.Timespan < remaining ? ttl.Timespan : remaining;
+            }
+            else
+            {
+                if (ttl.Timespan == TimeSpan.MaxValue)
+                {
+                    options.AbsoluteExpiration = DateTimeOffset.MaxValue;
+                }
+                else
+                {
+                    options.AbsoluteExpirationRelativeToNow = ttl.Timespan < remaining ? ttl.Timespan : remaining;
+                }
+            }
+
+            _cache.Set(key, value, options);
+        }
+
+        public Task<(bool, object)> TryGetAsync(string key, CancellationToken cancellationToken, bool continueOnCapturedContext)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            return Task.FromResult(TryGet(key));
+        }
+
+        public Task PutAsync(string key, object value, Ttl ttl, CancellationToken cancellationToken, bool continueOnCapturedContext)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            Put(key, value, ttl);
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Polly.Caching.Providers/ServiceCollectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Polly.Caching
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddMemoryCacheProvider(this IServiceCollection services)
+        {
+            return services.AddMemoryCache()
+                           .AddSingleton<IAsyncCacheProvider, MemoryCacheProvider>();
+        }
+    }
+}
